3031fabfdaa6553b471ea064b7443f897d8903ba
unknown <difalcoalex@gmail.com>
2023-12-14 23:43:06 +0100

    Modifiche alla Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <ul>
-            <li>
+        <ul style={{ listStyleType: "none" }}>
+            <li style={{ display: "inline", margin: "10px" }}>
                 <Link to={`/`}>home</Link>
             </li>
-            <li>
+            <li style={{ display: "inline", margin: "10px" }}>
                 <Link to={`/about`}>about</Link>
             </li>
-            <li>
+            <li style={{ display: "inline", margin: "10px" }}>
                 <Link to={`/contatti`}>contatti</Link>
             </li>
-            <li>
+            <li style={{ display: "inline", margin: "10px" }}>
                 <Link to={`/cards`}>card ver.1</Link>
             </li>
-            <li>
+            <li style={{ display: "inline", margin: "10px" }}>
                 <Link to={`/cards-children`}>card ver.2</Link>
             </li>
         </ul>
